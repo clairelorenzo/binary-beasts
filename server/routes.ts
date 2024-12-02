4229fc050e843e0d7fb4721d8342cbb127832dad
Cameron Young <cameron4young@gmail.com>
2024-12-02 14:03:22 -0500

    fixed routes (merge issue)

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,7 @@ import { ObjectId } from "mongodb";
 
 import { Router, getExpressRouter } from "./framework/router";
 
-import { Authing, Commenting, Friending, Posting, Sessioning, Tracking, Messaging } from "./app";
+import { Authing, Commenting, Friending, Messaging, Posting, Sessioning, Tracking } from "./app";
 import { CommentOptions } from "./concepts/commenting";
 import { PostOptions } from "./concepts/posting";
 import { SessionDoc } from "./concepts/sessioning";
@@ -323,6 +323,7 @@ class Routes {
     const userId = Sessioning.getUser(session);
     const response = await Messaging.deleteMessage(new ObjectId(conversationId), new ObjectId(messageId), userId);
     return response;
+  }
 
   @Router.get("/comments")
   @Router.validate(z.object({ postId: z.string().optional() }))
